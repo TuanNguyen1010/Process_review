544cd531bed39967241a337409e997f40333af34
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-20 11:30:52 +0000

    lower threshold test added

--- lib/music_filter.rb
+++ lib/music_filter.rb
@@ -1,3 +1,5 @@
 def music_filter(soundwave)
-  return soundwave
+  return [] if soundwave == []
+  return soundwave if soundwave[0] > 40
+  return [40]
 end 
--- spec/music_filter_spec.rb
+++ spec/music_filter_spec.rb
@@ -8,4 +8,8 @@ describe '#music_filter' do
   it 'returns 100 when input is within threshold' do
     expect(music_filter([100])).to eq([100])
   end 
+
+  it 'returns the lower threshold when input is below the lower threshold' do
+    expect(music_filter([20])).to eq([40])
+  end 
 end
