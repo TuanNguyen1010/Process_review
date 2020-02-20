d566d633d7635de4a01605af6465e34226700e57
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-20 11:40:17 +0000

    refactor music filter after adding upper threshold

--- lib/music_filter.rb
+++ lib/music_filter.rb
@@ -1,8 +1,9 @@
 LOWER_THRESHOLD = 40
+UPPER_THRESHOLD = 1000
 
 def music_filter(soundwave)
   return [] if soundwave == []
-  return [1000] if soundwave[0] > 1000
-  return soundwave if soundwave[0] > LOWER_THRESHOLD
-  return [LOWER_THRESHOLD]
+  return [UPPER_THRESHOLD] if soundwave[0] > UPPER_THRESHOLD
+  return [LOWER_THRESHOLD] if soundwave[0] < LOWER_THRESHOLD
+  return soundwave
 end 
